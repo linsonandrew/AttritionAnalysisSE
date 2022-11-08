db53b5efad366d5f2c4080f7bc469259baed5d56
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2022-11-08 21:45:58 +0530

    Update Exports.py

--- Exports.py
+++ Exports.py
@@ -1,4 +1,29 @@
 import streamlit as st
-def Exports_func():
-    st.write("Export")
-    st.session_state['lastpage'] = "Export"
+from PIL import Image
+
+# if 'page' not in st.session_state:
+
+#     st.session_state['page'] = 'home'
+
+# if 'inp' not in st.session_state:
+
+#     st.session_state['inp'] = 'default'
+
+# if 'login' not in st.session_state:
+
+#     st.session_state['login'] = 'No'
+
+
+if st.session_state['login'] == 'No':
+
+    st.title("Please go to login page and login first!")
+
+else:
+
+    num_of_files=7
+    temp = [str(i) for i in range(num_of_files)]
+    tabs_list = st.tabs(temp)
+    for i in range(num_of_files):
+        with tabs_list[i]:
+            img = Image.open(str(i)+".png")
+            st.image(img)
