f937df034f57e83735365dd13fb9ffdb596f97ea
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2022-11-15 10:46:34 +0530

    Add files via upload

---
+++ pages/Export.py
@@ -0,0 +1,79 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+
+st.set_page_config(
+    page_title="Attrition Solver",
+    page_icon="shark",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://github.com/wambyat/AttritionAnalysisSE',
+        'Report a bug': "https://github.com/wambyat/AttritionAnalysisSE",
+        'About': "This application will figure out which of your employees are most likely to leave. It can also tell you why and suggest a few basic solutions. Call 1800-this-a-proj for addition help"
+    }
+)
+
+# Initializing all the session_state variables
+#This is tecnically a leftover
+# reused to track which solutions have been chosen
+if 'page' not in st.session_state:
+
+    st.session_state['page'] = {}
+
+#This is tecnically a leftover
+# Reused to track which employee is selected
+if 'inp' not in st.session_state:
+
+    st.session_state['inp'] = 'default'
+
+#This is used to track if the user has logged in
+if 'login' not in st.session_state:
+
+    st.session_state['login'] = 'No'
+
+#This is used to track if the input is given.
+if 'input' not in st.session_state:
+
+    st.session_state['input'] = 'No'
+
+#This is also used to track user login
+#TODO Merge this and 'login'
+if 'login_test' not in st.session_state:
+
+    st.session_state['login_test'] = 0
+
+if 'model' not in st.session_state:
+    st.session_state['model'] = 'No'
+    
+
+if st.session_state['login'] == 'No':
+
+    st.title("Please go to login page and login first!")
+
+elif st.session_state['model'] == 'No':
+    st.title("Please give an input and run the model.")
+    st.subheader("Navigate to the input page to do this.")
+
+else:
+
+    st.title("Images you can use")
+
+    d= {"atrributes vs left":"This is a default desc","DEPARTMENTS VS LEFT":"THis is another default"}
+
+    temp = [str(i) for i in d.keys()]
+    tabs_list = st.tabs(temp)
+
+    for i in range(len(tabs_list)):
+        
+        with tabs_list[i]:
+            
+
+            img = Image.open(str(list(d.keys())[i])+".png")
+            if img.size[1]>500:
+                x = 650 / img.size[0]
+                img = img.resize((int(img.size[0]*x),(int(img.size[1]*x))))
+
+            st.subheader(d[str(list(d.keys())[i])])
+            st.image(img)
+            
